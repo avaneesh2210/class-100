b4188ff7fbc3a77df24984e22feadbe9a5cc01f7
avaneesh2210 <72684066+avaneesh2210@users.noreply.github.com>
2023-05-18 12:39:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var speechRecognition = window.webkitSpeechRecognition;
+var recognition = new speechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function run(event) {
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    console.log(content);
+
+    if(content == "take my selfie") {
+        console.log("taking selfie --- ");
+        speak();
+    }
+    else{
+        var speak_synth = window.speechSynthesis;
+speak_data2 = content;
+var utterThis2 = new SpeechSynthesisUtterance(speak_data2);
+speak_synth.speak(utterThis2);
+    }
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = "Taking you Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function () {
+        take_snapshot();
+        save();
+    }, 5000);
+}
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "selfie_image" src="'+data_uri+'">';
+    });
+}
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+}
